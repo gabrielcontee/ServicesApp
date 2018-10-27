c2caefe5393e083e42a55ee64ad2544bd13eb093
gabrielconte14 <gabrielconte14@gmail.com>
2018-10-27 20:41:42 -0300

    Implemented structure for list and place call

---
+++ ServicesApp/ServicesApp/New Group/Entity/Place.swift
@@ -0,0 +1,17 @@
+//
+//  Place.swift
+//  ServicesApp
+//
+//  Created by Gabriel Conte on 27/10/18.
+//  Copyright © 2018 Gabriel Conte. All rights reserved.
+//
+
+import Foundation
+
+struct Place: Decodable{
+    
+    let id: Int
+    let name: String?
+//    let description: String?
+//    let thumbnail: Image?
+}
---
+++ ServicesApp/ServicesApp/New Group/Entity/TasksList.swift
@@ -0,0 +1,15 @@
+//
+//  TasksList.swift
+//  ServicesApp
+//
+//  Created by Gabriel Conte on 27/10/18.
+//  Copyright © 2018 Gabriel Conte. All rights reserved.
+//
+
+import Foundation
+
+struct TasksList: Decodable{
+    
+    let id: Int
+    let name: String?
+}
